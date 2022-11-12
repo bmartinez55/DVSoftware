8cc6938b46ae6bb8ccde6de07d6ca860df6a65ac
Brian Martinez Castaneda <bmartinezc55@gmail.com>
2022-11-11 23:37:10 -0800

    [DV-19] 🚀 add firebase database domain and data layer (#38)

--- data/src/main/java/el/dv/data/di/scope/FayucaFinderDataModule.kt
+++ data/src/main/java/el/dv/data/di/scope/FayucaFinderDataModule.kt
@@ -1,9 +1,14 @@
 package el.dv.data.di.scope
 
+import el.dv.data.network.truck.repository.TruckRepositoryImpl
+import el.dv.domain.truck.repository.TruckRepository
 import org.koin.dsl.module
 
 /**
  * This module will hold all repository services
  */
 val fayucaFinderDataModule = module {
+    single<TruckRepository> {
+        TruckRepositoryImpl(truckApi = get(), dispatchers = get())
+    }
 }
--- data/src/main/java/el/dv/data/di/scope/FayucaFinderServiceModule.kt
+++ data/src/main/java/el/dv/data/di/scope/FayucaFinderServiceModule.kt
@@ -1,9 +1,31 @@
 package el.dv.data.di.scope
 
+import com.google.firebase.database.FirebaseDatabase
+import el.dv.data.network.truck.api.FayucaFinderTruckApi
+import el.dv.data.network.truck.api.TruckApi
+import el.dv.data.util.Const
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 /**
  * This module will hold all api level services
  */
 val fayucaFinderServiceModule = module {
+    /**
+     * Firebase Databases references
+     */
+    single(named("truckDB")) {
+        FirebaseDatabase.getInstance(Const.FIREBASE_DATABASE_TRUCK_URL)
+    }
+
+    single(named("truckDetailsDB")) {
+        FirebaseDatabase.getInstance(Const.FIREBASE_DATABASE_TRUCK_DETAILS_URL)
+    }
+
+    /**
+     * Api service classes
+     */
+    single<TruckApi> {
+        FayucaFinderTruckApi(truckDatabase = get(), truckDetailsDatabase = get())
+    }
 }
